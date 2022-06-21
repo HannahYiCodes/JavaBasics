e7718fbb34a3de36e1bb65d83918fc50f7ec951a
HannahYiCodes <hannahyi.students@careerdev.com>
2022-06-21 19:34:04 -0400

    JavaBasics Challenge Set 1L 1/3 - COMPLETE

---
+++ src/com/yi/challenge1/Main.java
@@ -0,0 +1,10 @@
+package com.yi.challenge1;
+
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "Hannah";
+        String lastName = "Yi";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+}
