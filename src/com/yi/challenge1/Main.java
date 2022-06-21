036301c25a208ea498aae6ed3ba85350aa4d842b
HannahYiCodes <hannahyi.students@careerdev.com>
2022-06-21 19:45:11 -0400

    JavaBasics Challenge Set 1: 2/3 - COMPLETE

--- src/com/yi/challenge1/Main.java
+++ src/com/yi/challenge1/Main.java
@@ -4,7 +4,12 @@ public class Main {
     public static void main(String[] args) {
         String firstName = "Hannah";
         String lastName = "Yi";
-        String fullName = firstName + " " + lastName;
+        String fullName = createFullName(firstName, lastName);
         System.out.println(fullName);
+        System.out.println(createFullName("First", "Last"));
+    }
+
+    private static String createFullName(String firstName, String lastName) {
+        return firstName + " " + lastName;
     }
 }
