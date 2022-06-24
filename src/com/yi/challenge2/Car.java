a7e4d0276a6277e2ae5b19d084267edd6b83cda1
HannahYiCodes <hannahyi.students@careerdev.com>
2022-06-23 21:39:37 -0400

    Car Challenge Complete

--- src/com/yi/challenge2/Car.java
+++ src/com/yi/challenge2/Car.java
@@ -1,7 +1,45 @@
 package com.yi.challenge2;
 
 public class Car {
-    public static String generateCarName(String carMake, String carModel, Integer carMileage, float gasTankPercent) {
-        return "Make: "+ carMake + ", " + "Model: " + carModel + ", " + "Mileage: " + carMileage + ", " + "Gas Tank Percent: " + gasTankPercent*100 + "%";
+    private String make;
+    private String model;
+    private int mileage;
+    private float gasTankPercent;
+
+    public Car(String make, String model, int mileage, float gasTankPercent) {
+        this.make = make;
+        this.model = model;
+        this.mileage = mileage;
+        this.gasTankPercent = gasTankPercent;
+    }
+    public String getMake() {
+        return make;
+    }
+    public void getMake(String make) {
+        this.make = make;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void getModel(String model) {
+        this.model = model;
+    }
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void getMileage(int mileage) {
+        this.mileage = mileage;
+    }
+    public float getGasTankPercent() {
+        return gasTankPercent;
+    }
+    public void getGasTankPercent(float gasTankPercent) {
+        this.gasTankPercent = gasTankPercent;
+    }
+
+    @Override
+    public String toString() {
+        return "Make: " + make + ", Model: " + model + ", Mileage: " + mileage + ", Gas Tank: " + gasTankPercent*100 + "%";
     }
 }
--- src/com/yi/challenge2/Main.java
+++ src/com/yi/challenge2/Main.java
@@ -2,12 +2,17 @@ package com.yi.challenge2;
 
 public class Main {
     public static void main(String[] args) {
-        Car nameCarGen = new Car();
-        String generatedCarName1 = Car.generateCarName("Camaro 2SS", "Chevy", 10000, .10f);
-        String generatedCarName2 = Car.generateCarName("Mustang GT", "Ford", 30000, .50f);
-        String generatedCarName3 = Car.generateCarName("Hellcat", "Dodge", 100, .90f);
-        System.out.println(generatedCarName1);
-        System.out.println(generatedCarName2);
-        System.out.println(generatedCarName3);
+        Car car1 = new Car("Chevy", "Camaro 2SS", 10000, .10f);
+        Car car2 = new Car("Ford", "Mustang GT", 30000, .50f);
+        Car car3 = new Car("Dodge", "Hellcat", 100, .90f);
+
+//        System.out.println("Make: " + car1.make + ", " + "Model: " + car1.model + ", " + "Mileage: " + car1.mileage + ", " + "Gas Tank Percent: " + car1.gasTankPercent*100 + "%");
+//        System.out.println("Make: " + car2.make + ", " + "Model: " + car2.model + ", " + "Mileage: " + car2.mileage + ", " + "Gas Tank Percent: " + car2.gasTankPercent*100 + "%");
+//        System.out.println("Make: " + car3.make + ", " + "Model: " + car3.model + ", " + "Mileage: " + car3.mileage + ", " + "Gas Tank Percent: " + car3.gasTankPercent*100 + "%");
+
+        Car[] carsArray = {car1, car2, car3};
+        for (int i = 0; i < carsArray.length; i++) {
+            System.out.println(carsArray[i]);
+        }
     }
 }
