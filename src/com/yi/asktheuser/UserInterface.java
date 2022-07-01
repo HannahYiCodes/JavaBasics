f25b2865c0a5fe4bf73b72e2f3bda343ed13946a
HannahYiCodes <hannahyi.students@careerdev.com>
2022-06-30 20:14:14 -0400

    Car Factory

--- src/com/yi/asktheuser/UserInterface.java
+++ src/com/yi/asktheuser/UserInterface.java
@@ -6,11 +6,23 @@ public class UserInterface {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        String name = askUserName();
+        int age = askUserAge();
+        System.out.println("Final Report:\nName: " + name +"\nAge: " + age); // after all the questions, it will display this
+    }
+
+    private static int askUserAge() {
+        System.out.print("What is your age?\nAge: ");
+        int userAge = scanner.nextInt();
+        System.out.println("Your age; " + userAge);
+        return userAge;
+    }
+
+    private static String askUserName () {
         System.out.print("What is your name?\nName: ");
         String userName = scanner.nextLine();
-
         System.out.println("Your name; " + userName);
-
+        return userName;
     }
 }
 
