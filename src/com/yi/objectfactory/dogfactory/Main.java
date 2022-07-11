1747b86b6f74caef01848ab183e350f4125b1222
HannahYiCodes <106121229+HannahYiCodes@users.noreply.github.com>
2022-07-11 17:52:37 -0400

    Update Main.java

--- src/com/yi/objectfactory/dogfactory/Main.java
+++ src/com/yi/objectfactory/dogfactory/Main.java
@@ -8,6 +8,11 @@ public class Main {
         String dogBreed = UI.readString("What is your dog's breed(s)?");
         int adoptionDate = UI.readInt("What year did you acquire your best friend?", 1900, 9000);
         short dogAge = UI.readShort("How old is your dog?", (short) 0, (short) 30);
+        short dogWeight = UI.readShort("How many pounds is your dog?", (short) 0, (short) 200);
         boolean dogSex = UI.readBoolean("Is your dog a male?");
+
+        Dog generatedDog = new Dog(dogName, dogBreed, adoptionDate, dogAge, dogWeight, dogSex);
+        System.out.println(generatedDog);
+
     }
 }
