38ab1b73ec6dd0a28e9c76230682274fef03697a
HannahYiCodes <hannahyi.students@careerdev.com>
2022-07-12 21:31:25 -0400

    Factory Program

--- src/com/yi/objectfactory/dogfactory/Dog.java
+++ src/com/yi/objectfactory/dogfactory/Dog.java
@@ -1,20 +1,26 @@
 package com.yi.objectfactory.dogfactory;
 
+import com.yi.objectfactory.UI;
+
 public class Dog {
     private String dogName;
     private String dogBreed;
-    private int adoptionDate;
+    private int adoptionYear;
     private short dogAge;
     private short dogWeight;
-    private boolean dogSex;
+    private String dogSex;
+    private boolean dogStatus;
+    private char dogOwner;
 
-    public Dog(String dogName, String dogBreed, int adoptionDate, short dogAge, short dogWeight, boolean dogSex) {
+    public Dog(String dogName, String dogBreed, int adoptionYear, short dogAge, short dogWeight, String dogSex, boolean dogStatus, char dogOwner) {
         this.dogName = dogName;
         this.dogBreed = dogBreed;
-        this.adoptionDate = adoptionDate;
+        this.adoptionYear = adoptionYear;
         this.dogAge = dogAge;
         this.dogWeight = dogWeight;
         this.dogSex = dogSex;
+        this.dogStatus = dogStatus;
+        this.dogOwner = dogOwner;
     }
 
     public String getDogName() {
@@ -26,7 +32,7 @@ public class Dog {
     }
 
     public int getAdoptionDate() {
-        return adoptionDate;
+        return adoptionYear;
     }
 
     public short getDogAge() {
@@ -37,19 +43,28 @@ public class Dog {
         return dogWeight;
     }
 
-    public boolean isDogSex() {
+    public String getDogSex() {
         return dogSex;
     }
 
+    public boolean getDogStatus() { return dogStatus; }
+
+    public char getDogOwner() { return dogOwner;}
+
     @Override
     public String toString() {
-        return "Dog{" +
-                "dogName='" + dogName + '\'' +
-                ", dogBreed='" + dogBreed + '\'' +
-                ", adoptionDate=" + adoptionDate +
-                ", dogAge=" + dogAge +
-                ", dogWeight=" + dogWeight +
-                ", dogSex=" + dogSex +
-                '}';
+        final StringBuilder sb = new StringBuilder("Registered Dog");
+        sb.append("\nDog Name: '").append(dogName).append('\'');
+        sb.append("\nBreed: ").append(dogBreed).append('\'');
+        sb.append("\nAdoption Year: ").append(adoptionYear);
+        sb.append("\nAge: ").append(dogAge);
+        sb.append("\nWeight: ").append(dogWeight);
+        sb.append("\nSex: ").append(dogSex);
+        sb.append("\nFirst-timer: ").append(dogStatus ? "Yes! Welcome!" : "No, but Welcome Back!");
+        sb.append("\nFirst initial of owner: ").append(dogOwner);
+        return sb.toString();
     }
 }
+
+//TODO: public static ArrayList<String> readListOfStrings (question, min, max); // OBJECT NAME SELECTION?
+//TODO: public static String selectFromListOfString (question, ArrayList<String>)
